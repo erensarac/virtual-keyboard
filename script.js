7aa919781a265a2473217760184444df2089432d
Eren Saraç <esarac@outlook.com.tr>
2022-05-26 19:14:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+const testArea = document.querySelector('#testArea');
+
+document.addEventListener("keyup", (e) => {
+  console.log('sa');
+  let keyId = `#key-${e.key}`;
+  let keyElement = document.querySelector(keyId);
+  keyElement.classList.add('active');
+  setTimeout(() => {
+    keyElement.classList.remove('active');
+  }, 100);
+  testArea.focus();
+})
+
+document.addEventListener("keypress", (e) => {
+  let keyId = `#key-${e.key}`;
+  let keyElement = document.querySelector(keyId);
+  keyElement.classList.add('active');
+  testArea.focus();
+})
+
+document.addEventListener('keydown', (e) => {
+  let keyId = `#key-${e.key}`;
+  let keyElement = document.querySelector(keyId);
+  keyElement.classList.remove('active');
+  testArea.focus();
+})
